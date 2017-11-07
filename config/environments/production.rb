4650aa91b348a1a866e65c268166f7b4ec7a8fd3
Jonathan Serafini <jonathan.serafini@gmail.com>
2017-11-07 17:32:14 +0100

    config action cable and websockets for production to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,6 +40,9 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  config.web_socket_server_url = "wss://twweams22.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://twweams22.herokuapp.com', 'http://twweams22.herokuapp.com']
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
